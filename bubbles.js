5cb09130b57b6e074c2fbbc5d592bddd2fa4c73a
easrng <easrng@gmail.com>
2022-02-11 11:04:14 -0500

    Update bubbles.js

--- bubbles.js
+++ bubbles.js
@@ -1,3 +1,3 @@
-export default function bubbles(i=1){
+export default (function bubbles(i=0){
   return Object.assign(Object.setPrototypeOf(()=>bubbles(i+1), Promise.prototype), {then:f=>f(i)})
-}
+})()
